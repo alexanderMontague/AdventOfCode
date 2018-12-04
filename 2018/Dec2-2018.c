fb25aebd4445a7a1ae967af155764a972e27c590
Alex Montague <xturnermonty@gmail.com>
2018-12-03 20:46:44 -0500

    dec 2

--- 2018/Dec2-2018.c
+++ 2018/Dec2-2018.c
@@ -7,23 +7,24 @@ int main()
 {
   // Vars
   FILE *fileStream = NULL;
+  char stringArray[300][50];
   char inputString[100];
+  int lineCount = 0;
   int doubleCharCount = 0;
   int tripleCharCount = 0;
-  // fileStream = fopen("./inputs/Dec2-2018.txt", "r");
   fileStream = fopen("./inputs/Dec2-2018.txt", "r");
 
   while (fgets(inputString, 100, fileStream) != NULL)
   {
     inputString[strlen(inputString) - 1] = '\0';
-    printf("%s\n", inputString);
+    lineCount++;
+    strcpy(stringArray[lineCount - 1], inputString);
 
     int *letterArray = malloc(250 * sizeof(int));
 
     for (int i = 0; i < strlen(inputString); i++)
     {
       letterArray[(int)inputString[i]]++;
-      printf("Letter: %c  |  Freq: %d\n", inputString[i], letterArray[(int)inputString[i]]);
     }
 
     bool foundDouble = false;
@@ -43,7 +44,50 @@ int main()
     }
   }
 
+  char diffWord[50];
+  char formattedWord[50];
+  int newDiffCharIndex = 0;
+
+  for (int x = 0; x < lineCount; x++)
+  {
+    for (int y = x; y < lineCount; y++)
+    {
+      int diffChars = 0;
+      int diffWordOneIndex = 0;
+      int diffWordTwoIndex = 0;
+      int diffCharIndex = 0;
+
+      for (int z = 0; z < strlen(stringArray[x]); z++)
+      { // O(n^3) hurts me
+        if (stringArray[x][z] != stringArray[y][z])
+        {
+          diffChars++;
+          diffCharIndex = z;
+          diffWordOneIndex = x;
+          diffWordTwoIndex = y;
+        }
+      }
+      if (diffChars == 1)
+      {
+        printf("Diff Words: %s | %s\n", stringArray[diffWordOneIndex], stringArray[diffWordTwoIndex]);
+        strcpy(diffWord, stringArray[diffWordOneIndex]);
+        newDiffCharIndex = diffCharIndex;
+      }
+    }
+  }
+
+  int count = 0;
+  for (int a = 0; a < strlen(diffWord); a++)
+  {
+    if (a != newDiffCharIndex)
+    {
+      formattedWord[count] = diffWord[a];
+      count++;
+    }
+  }
+
   printf("Doubles: %d  |  Triples: %d\n", doubleCharCount, tripleCharCount);
+  printf("New Word: %s\n", formattedWord);
 
   return 0;
 }
